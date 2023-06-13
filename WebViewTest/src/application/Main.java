ea38bd892ba3f18564da1469d7d40501d8e9df69
LamboLaure <laurencezobl@gmail.com>
2023-06-13 11:12:33 +0200

    Kalender und  Textfeld

--- WebViewTest/src/application/Main.java
+++ WebViewTest/src/application/Main.java
@@ -4,12 +4,16 @@ package application;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -43,10 +47,7 @@ public class Main extends Application {
 						// Must call super
 						super.updateItem(item, empty);
 
-						// Disable all future date cells
-						if (item.isAfter(LocalDate.now())) {
-							this.setDisable(true);
-						}
+	
 
 						// Show Weekends in blue color
 						DayOfWeek day = DayOfWeek.from(item);
@@ -60,30 +61,47 @@ public class Main extends Application {
 		// Set the day cell factory
 		birthDate.setDayCellFactory(dayCellFactory);
 	
+		
+		
+		Label r = new Label("Text eingeben"); 
+		
+		HBox root = new HBox(100); 
+		root.getChildren().add(r);
+		root.getChildren().add(birthDate);
+		
+		
+		
 		Label l = new Label("Text eingeben"); 
 		TextField t = new TextField(); 
 		
-		
-		
-		HBox root = new HBox(new Label("Birth Date:"), birthDate); 
-		root.setStyle("-fx-padding: 10;" + 
-		              "-fx-border-style: solid inside;" + 
-		              "-fx-border-width: 5;" +
-		              "-fx-border-insets: 5;" + 
-		             "-fx-border-radius: 5;" + 
-		             "-fx-border-color: blue;");
-		
-		HBox h = new HBox(100);
+		HBox h = new HBox(100); 
 		h.getChildren().add(l); 
 		h.getChildren().add(t); 
 		
-		root.getChildren().add(h); 
 		
-		Scene scene = new Scene(root);
 		
-		stage.setScene(scene);
-		stage.setTitle("Using DatePicker Control");
-		stage.show();
-		stage.sizeToScene();
+		Button btn = new Button("OK"); 
+		
+		btn.setOnAction(null);
+		
+		
+		HBox b = new HBox(100); 
+		b.getChildren().add(btn);
+		
+		
+		GridPane grid = new GridPane(); 
+		grid.setPadding(new Insets(5,5,5,5));
+		grid.setHgap(5);
+		grid.setVgap(5);
+		grid.add(root, 0, 0);
+		grid.add(h, 0, 1);
+		grid.add(b, 1, 2);
+		
+		Scene scene = new Scene(grid, 450, 450);
+		Stage s = new Stage();
+		s.setScene(scene);
+		s.setTitle("KalenderToDoList");
+		s.show();
+
 	}
 }
--- WebViewTest/src/module-info.java
+++ WebViewTest/src/module-info.java
@@ -2,6 +2,7 @@ module WebViewTest {
 	requires javafx.controls;
 	requires javafx.web;
 	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
